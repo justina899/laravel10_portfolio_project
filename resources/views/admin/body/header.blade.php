d6b7a8d0b87b1ccb7dcd55f9068407eebdbc503b
Justina <justina.babrauskiene@gmail.com>
2024-03-12 13:46:49 +0200

    admin profile and image update

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -46,27 +46,31 @@
                 </button>
             </div>
 
+            @php
+                $id = Auth::user()->id;
+                $adminData = App\Models\User::find($id);
+            @endphp
+
+
             <div class="dropdown d-inline-block user-dropdown">
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="rounded-circle header-profile-user" src="{{asset('backend/assets/images/users/avatar-1.jpg')}}"
+                    <img class="rounded-circle header-profile-user" src="{{(!empty($adminData->profile_image))? url('upload/admin_images/'.$adminData->profile_image):url('upload/no_image.jpg')}}"
                         alt="Header Avatar">
-                    <span class="d-none d-xl-inline-block ms-1">Julia</span>
-                    <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
+                    <span class="d-xl-inline-block ms-1">{{$adminData->name}}</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                        <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                    </svg>
                 </button>
+
                 <div class="dropdown-menu dropdown-menu-end">
                     <!-- item-->
-                    <a class="dropdown-item" href="#">
-                        <i class="ri-user-line align-middle me-1"></i>Profile</a>
-                    <a class="dropdown-item" href="#">
-                        <i class="ri-wallet-2-line align-middle me-1"></i>My Wallet</a>
-                    <a class="dropdown-item d-block" href="#">
-                        <span class="badge bg-success float-end mt-1">11</span><i class="ri-settings-2-line align-middle me-1"></i> Settings</a>
-                    <a class="dropdown-item" href="#">
-                        <i class="ri-lock-unlock-line align-middle me-1"></i>Lock screen</a>
+                    <a class="dropdown-item" href="{{ route('admin.profile') }}"><i class="ri-user-line align-middle me-1"></i> Profile</a>
+                    <a class="dropdown-item" href="#"><i class="ri-wallet-2-line align-middle me-1"></i> My Wallet</a>
+                    <a class="dropdown-item d-block" href="#"><span class="badge bg-success float-end mt-1">11</span><i class="ri-settings-2-line align-middle me-1"></i> Settings</a>
+                    <a class="dropdown-item" href="#"><i class="ri-lock-unlock-line align-middle me-1"></i> Lock screen</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item text-danger" href="{{route('admin.logout')}}">
-                        <i class="ri-shut-down-line align-middle me-1 text-danger"></i>Logout</a>
+                    <a class="dropdown-item text-danger" href="{{ route('admin.logout') }}"><i class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('/', function () {
 //Admin all route
 Route::controller(AdminController::class)->group(function () {
     Route::get('/admin/logout', 'destroy')->name('admin.logout');
+    Route::get('/admin/profile', 'Profile')->name('admin.profile');
+    Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
+    Route::post('/store/profile', 'StoreProfile')->name('store.profile');
 });
 
 Route::get('/dashboard', function () {
