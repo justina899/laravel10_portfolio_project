fec66f1e925ba7f00bd72c159c59a3158f3bb415
Justina <justina.babrauskiene@gmail.com>
2024-03-18 13:30:13 +0200

    backend: home page slider

---
+++ app/Http/Controllers/Home/HomeSliderController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+//require './vendor/autoload.php';
+
+use App\Http\Controllers\Controller;
+use App\Models\HomeSlide;
+use Illuminate\Http\Request;
+use App\Models\HomeSlider;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\Laravel\Facades\Image;
+
+
+
+class HomeSliderController extends Controller
+{
+    public function HomeSlider(){
+        $homeslide = HomeSlide::find(1);
+        return view('admin.home_slide.home_slide_all', compact('homeslide'));
+    } //End method
+
+    public function UpdateSlider(Request $request){
+        $slide_id = $request->id;
+
+
+        if ($request->file('home_slide')){
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()).'.'.$request->file('home_slide')->getClientOriginalExtension();
+            $img = $manager->read($request->file('home_slide'));
+            $img = $img->resize(636, 852);
+            $img->toJpeg(80)->save('upload/home_slide/'.$name_gen);
+            $save_url = 'upload/home_slide/'.$name_gen;
+
+            HomeSlide::findOrFail($slide_id)->update([
+                'title' => $request->title,
+                'short_title' => $request->short_title,
+                'video_url' => $request->video_url,
+                'home_slide' => $save_url,
+            ]);
+
+            $notification = array(
+                'message' => 'Home Slide updated with image successfully',
+                'alert-type' => 'info'
+            );
+
+            return redirect()->back()->with($notification);
+        } else{
+            HomeSlide::findOrFail($slide_id)->update([
+                'title' => $request->title,
+                'short_title' => $request->short_title,
+                'video_url' => $request->video_url,
+            ]);
+
+            $notification = array(
+                'message' => 'Home Slide updated without image successfully',
+                'alert-type' => 'info'
+            );
+
+            return redirect()->back()->with($notification);
+        } //End else
+    } //End method
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Home\HomeSliderController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +30,12 @@ Route::controller(AdminController::class)->group(function () {
     Route::post('/store/profile', 'StoreProfile')->name('store.profile');
     Route::get('/change/password', 'ChangePassword')->name('change.password');
     Route::post('/update/password', 'UpdatePassword')->name('update.password');
+});
+
+//Home Slide all route
+Route::controller(HomeSliderController::class)->group(function () {
+    Route::get('/home/slide', 'HomeSlider')->name('home.slide');
+    Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
 });
 
 Route::get('/dashboard', function () {
