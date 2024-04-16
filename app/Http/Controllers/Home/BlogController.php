1bfffc7bae777f6e3e84bbe0ef15fa690f8f9be1
Justina <justina.babrauskiene@gmail.com>
2024-04-16 17:34:50 +0300

    frontend blog show setup

--- app/Http/Controllers/Home/BlogController.php
+++ app/Http/Controllers/Home/BlogController.php
@@ -113,5 +113,25 @@ class BlogController extends Controller
         return redirect()->back()->with($notification);
     } //End method
 
+    public function BlogDetails($id){
+        $allblogs = Blog::latest()->limit(5)->get();
+        $categories = BlogCategory::orderBy('blog_category', 'ASC')->get();
+        $blogs = Blog::findOrFail($id);
+        return view('frontend.blog_details', compact('blogs', 'allblogs', 'categories'));
+    } //End method
+
+    public function CategoryBlog($id){
+        $blogpost = Blog::where('blog_category_id', $id)->orderBy('id', 'DESC')->get();
+        $allblogs = Blog::latest()->limit(5)->get();
+        $categories = BlogCategory::orderBy('blog_category', 'ASC')->get();
+        $categoryname = BlogCategory::findOrFail($id);
+        return view('frontend.cat_blog_details', compact('blogpost', 'allblogs', 'categories', 'categoryname'));
+    } //End method
+
+    public function HomeBlog(){
+        $allblogs = Blog::latest()->get();
+        $categories = BlogCategory::orderBy('blog_category', 'ASC')->get();
+        return view('frontend.blog', compact('allblogs', 'categories'));
+    } //End method
 
 }
